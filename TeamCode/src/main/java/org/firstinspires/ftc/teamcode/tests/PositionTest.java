a5171affc6d2630d668a40d91a70bbf05032794a
Pioneer Robotics <pioneer.robotics.12589@gmail.com>
2023-10-20 14:28:19 -0400

    display rotation and translation are working :D

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/PositionTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/PositionTest.java
@@ -36,15 +36,21 @@ public class PositionTest extends OpMode {
 
     double side_length = 5;
 
-    double scale = 0.0737;
+    double scale;
+
+    public static double lateral_offset;
 
     @Override
     public void init() {
 
-        wheel_circ = 157.08; // mm
-        track_width = 280; // mm distance between drive wheels
-        forward_offset = 145; // mm distance from center of robot to perp wheel
+        wheel_circ = 15.708; // cm
+        track_width = 29.53/2; // cm distance between drive wheels
+        forward_offset = -14.5; // cm distance from center of robot to perp wheel
         ticksPerRev = 8192;
+
+        lateral_offset = 0.5;
+
+        scale = wheel_circ / ticksPerRev;
 
         BHI260IMU.Parameters parameters = new IMU.Parameters(
                 new RevHubOrientationOnRobot(
@@ -55,7 +61,7 @@ public class PositionTest extends OpMode {
 
         imu = hardwareMap.get(BHI260IMU.class, "imu");//omkar is gay
         imu.initialize(parameters);
-
+//peepeepoopoo lol
         frontLeft  = hardwareMap.get(DcMotor.class, "frontLeft");
         frontRight = hardwareMap.get(DcMotor.class, "frontRight");
         backLeft = hardwareMap.get(DcMotor.class, "backLeft");
@@ -76,19 +82,15 @@ public class PositionTest extends OpMode {
 
         TelemetryPacket packet = new TelemetryPacket();
         FtcDashboard dashboard = FtcDashboard.getInstance();
-        double[] xs = {(side_length * Math.cos(theta) - side_length * Math.sin(theta)) + 5,
-                (-side_length * Math.cos(theta) - side_length * Math.sin(theta)) + 5,
-                (side_length * Math.sin(theta) - side_length * Math.cos(theta)) + 5,
-                (side_length * Math.cos(theta) + side_length * Math.sin(theta)) + 5};
+        double[] xs = {(side_length * Math.cos(theta) - side_length * Math.sin(theta)) + x,
+                (-side_length * Math.cos(theta) - side_length * Math.sin(theta)) + x,
+                (-side_length * Math.cos(theta) + side_length * Math.sin(theta)) + x,
+                (side_length * Math.cos(theta) + side_length * Math.sin(theta)) + x};
 
-//        double[] xs = {scale * x + side_length, scale * x + side_length, scale * x + -side_length, scale * x + -side_length};
-
-//        double[] ys = {scale * y + side_length, scale * y + -side_length, scale * y + -side_length, scale * y + side_length};
-
-        double[] ys = {(side_length * Math.sin(theta) + side_length * Math.cos(theta)) + 5,
-                (-side_length * Math.sin(theta) + side_length * Math.cos(theta)) + 5,
-                (-side_length * Math.cos(theta) - side_length * Math.sin(theta)) + 5,
-                (side_length * Math.sin(theta) - side_length * Math.cos(theta)) + 5};
+        double[] ys = {(side_length * Math.sin(theta) + side_length * Math.cos(theta)) + y,
+                (-side_length * Math.sin(theta) + side_length * Math.cos(theta)) + y,
+                (-side_length * Math.sin(theta) - side_length * Math.cos(theta)) + y,
+                (side_length * Math.sin(theta) - side_length * Math.cos(theta)) + y};
 
         packet.fieldOverlay().fillPolygon(xs, ys).setFill("blue");
 
@@ -101,20 +103,20 @@ public class PositionTest extends OpMode {
         backLeft.setPower(move - turn - strafe);
         backRight.setPower(move + turn + strafe);
 
-        double delta_ticks_left = (frontLeft.getCurrentPosition() - prev_ticks_left) / ticksPerRev * wheel_circ;
-        double delta_ticks_right = (frontRight.getCurrentPosition() - prev_ticks_right) / ticksPerRev * wheel_circ;
-        double delta_ticks_back = (backRight.getCurrentPosition() - prev_ticks_back) / ticksPerRev * wheel_circ;
+        double delta_ticks_left = (frontLeft.getCurrentPosition() - prev_ticks_left);
+        double delta_ticks_right = (frontRight.getCurrentPosition() - prev_ticks_right);
+        double delta_ticks_back = (backRight.getCurrentPosition() - prev_ticks_back);
 
-        dtheta = ((delta_ticks_left - delta_ticks_right) / track_width) * 2;
-        dx_center = (delta_ticks_left + delta_ticks_right) / 2;
-        dx_perpendicular = delta_ticks_back - (forward_offset * dtheta);
+        dtheta = ((delta_ticks_left - delta_ticks_right) / track_width) * scale;
+        dx_center = ((delta_ticks_left + delta_ticks_right) / 2) * scale;
+        dx_perpendicular = (delta_ticks_back - (forward_offset * ((delta_ticks_left - delta_ticks_right) / track_width))) * scale * lateral_offset;
 
         dx = dx_center * Math.cos(theta) - dx_perpendicular * Math.sin(theta);
         dy = dx_center * Math.sin(theta) + dx_perpendicular * Math.cos(theta);
 
         x += dx;
         y += dy;
-        theta += dtheta;
+        theta += -1 * dtheta;
 
 
         telemetry.addData("ticks back", prev_ticks_back);
